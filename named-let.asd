5c7f9dc720f03431acd6815c3611e05ed1653043
Joe Marshall <joseph.marshall@nutanix.com>
2025-02-18 11:09:24 -0800

    Update.

--- named-let.asd
+++ named-let.asd
@@ -1,5 +1,15 @@
 ;;; -*- Lisp -*-
 
 (defsystem "named-let"
+  :author "Joe Marshall <eval.apply@gmail.com>"
+  :bug-tracker "https://github.com/jrm-code-project/named-let/issues"
+  :description "Named LET special form."
+  :homepage "https://github.com/jrm-code-project/named-let/"
+  :license "MIT"
+  :long-description "A named variant of the standard LET special form that allows a name to be provided before the binding list.  The name is bound to the lambda that binds the variables making it possible to recursively call the LET expression with new bindings."
+  :mailto "eval.apply@gmail.com"
+  :maintainer "Joe Marshall <eval.apply@gmail.com>"
+  :source-control (:git "https://github.com/jrm-code-project/named-let.git")
+  :version "1.0.0"
   :depends-on ()
   :components ((:file "named-let")))
--- named-let.lisp
+++ named-let.lisp
@@ -13,9 +13,9 @@
 
 (defmacro flambda ((&rest arglist) &body body)
   "A lambda that binds its arguments in the function namespace."
-  (let ((vars (map 'list (lambda (arg)
-                           (gensym (concatenate 'string (symbol-name arg) "-")))
-                   arglist)))
+  (cl:let ((vars (map 'list (lambda (arg)
+                              (gensym (concatenate 'string (symbol-name arg) "-")))
+                      arglist)))
     `(lambda ,vars
        (flet ,(map 'list (lambda (arg var)
                            `(,arg (&rest args)
@@ -28,6 +28,7 @@
   `(labels ((,name ,arglist ,@body))
      #',name))
 
+;; Override the LET special form with an extended syntax.
 (defmacro let (name-or-bindings bindings-or-first &body body)
   "The LET special form with extended syntax.  A symbolic NAME may be provided before the binding list
 that is bound to the function performing the LET binding so that the function may be called again."
@@ -43,46 +44,34 @@ that is bound to the function performing the LET binding so that the function ma
                  ((null (cdr binding)) 'nil)
                  ((not (consp (cdr binding))) (error "Bad syntax: LET"))
                  ((null (cddr binding)) (cadr binding))
-                 (t `(progn ,@(cdr binding))))))
+                 (t `(progn ,@(cdr binding)))))
 
-    (cond ((symbolp name-or-bindings)
-           `(funcall (named-lambda ,name-or-bindings
-                                   ,(map 'list #'binding-name
-                                          bindings-or-first)
-                                   ,@body)
-                     ,@(map 'list #'binding-value
-                            bindings-or-first)))
-          ((or (null name-or-bindings)
+         (get-symbol (name)
+           (cl:let ((series-package (find-package "SERIES")))
+             (if series-package
+                 (cl:let ((symbol (find-symbol name series-package)))
+                   (if (and symbol
+                            (fboundp symbol))
+                       symbol
+                       (find-symbol name (find-package "COMMON-LISP"))))
+                 (find-symbol name (find-package "COMMON-LISP"))))))
+
+    (cond ((or (null name-or-bindings)
                (consp name-or-bindings))
-           `(,(cl:let ((series-package (find-package "SERIES")))
-                      (if series-package
-                          (cl:let ((let-symbol (find-symbol "LET" series-package)))
-                                  (if (and let-symbol
-                                           (fboundp let-symbol))
-                                      let-symbol
-                                    'cl:let))
-                        'cl:let))
-             ,name-or-bindings
+           `(,(get-symbol "LET")
+             ,(map 'list (lambda (binding)
+                          (list (binding-name binding) (binding-value binding)))
+                   name-or-bindings)
              ,bindings-or-first
              ,@body))
+          ((symbolp name-or-bindings)
+           `(,(get-symbol "FUNCALL") (named-lambda ,name-or-bindings
+                                        ,(map 'list #'binding-name
+                                          bindings-or-first)
+                                      ,@body)
+                     ,@(map 'list #'binding-value
+                            bindings-or-first)))
           (t (error "Bad syntax: LET")))))
 
 (defmacro named-let (name bindings &body body)
-  (flet ((binding-name (binding)
-           (cond ((symbolp binding) binding)
-                 ((not (consp binding)) (error "Bad synax: NAMED-LET"))
-                 ((symbolp (car binding)) (car binding))
-                 (t (error "Bad syntax: NAMED-LET"))))
-
-         (binding-value (binding)
-           (cond ((symbolp binding) 'nil)
-                 ((not (consp binding)) (error "Bad syntax: NAMED-LET"))
-                 ((null (cdr binding)) 'nil)
-                 ((not (consp (cdr binding))) (error "Bad syntax: NAMED-LET"))
-                 ((null (cddr binding)) (cadr binding))
-                 (t `(progn ,@(cdr binding))))))
-
-    `(funcall (named-lambda ,name ,(map 'list #'binding-name bindings)
-                ,@body)
-              ,@(map 'list #'binding-value
-                     bindings))))
+  `(let ,name ,bindings ,@body))
